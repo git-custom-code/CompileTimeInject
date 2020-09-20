0d80dd3b2a36ead1583d176a570d4f5718a3bff5
Stefan Loeffler <s.loeffler@custom-co.de>
2020-09-20 22:45:26 +0200

    added functionality to create a compilation from a collection of (string) code files

--- src/Test.CompileTimeInject.ContainerGenerator/Syntax/CompilationBuilder.cs
+++ src/Test.CompileTimeInject.ContainerGenerator/Syntax/CompilationBuilder.cs
@@ -1,7 +1,10 @@
 namespace CustomCode.CompileTimeInject.ContainerGenerator.Syntax
 {
+    using CustomCode.CompileTimeInject.Annotations;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
+    using System.Collections.Generic;
+    using System.Reflection;
 
     /// <summary>
     /// A set of helper methods for easier creation of in-memory <see cref="Compilation"/>s.
@@ -18,6 +21,41 @@ namespace CustomCode.CompileTimeInject.ContainerGenerator.Syntax
         {
             var emptyAssembly = CSharpCompilation.Create("TestAssembly");
             return emptyAssembly;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Compilation"/> that represents a c# assembly with the
+        /// given <paramref name="sourceCodeFiles"/>.
+        /// </summary>
+        /// <param name="sourceCodeFiles">
+        /// A collection of c# source code files that should be included in the created assembly.
+        /// </param>
+        /// <returns> The created c# assembly. </returns>
+        public static Compilation CreateAssemblyWithCode(params string[] sourceCodeFiles)
+        {
+            var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var netStandard = MetadataReference.CreateFromFile(Assembly.Load("netstandard, Version=2.0.0.0").Location);
+            var systemRuntime = MetadataReference.CreateFromFile(Assembly.Load("System.Runtime, Version=5.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a").Location);
+            var annotations = MetadataReference.CreateFromFile(typeof(ExportAttribute).Assembly.Location);
+
+            var syntaxTrees = new List<SyntaxTree>();
+            foreach(var sourceCode in sourceCodeFiles)
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode, new CSharpParseOptions());
+                syntaxTrees.Add(syntaxTree);
+            }
+
+            var options = new CSharpCompilationOptions(
+               OutputKind.DynamicallyLinkedLibrary,
+               optimizationLevel: OptimizationLevel.Debug);
+
+            var assembly = CSharpCompilation.Create(
+                assemblyName: "TestAssembly",
+                syntaxTrees: syntaxTrees,
+                references: new[] { mscorlib, netStandard, systemRuntime, annotations },
+                options: options);
+
+            return assembly;
         }
 
         #endregion
